3f03e29b907a074d5b778fd0c575e5838e7bd599
glinka21 <oliver.bech21@gmail.com>
2026-08-02 09:45:32 +0200

    Refactor config.js to use module exports
    Replaced JSON configuration with module exports for club details.

--- config.js
+++ config.js
@@ -1,14 +1,21 @@
-{
-  "name": "frem-dbu-calendar",
-  "version": "1.0.0",
-  "description": "Boldklubben Frem 1. hold kampprogram til Google Calendar",
-  "main": "update.js",
-  "scripts": {
-    "update": "node update.js"
-  },
-  "dependencies": {
-    "axios": "^1.7.9",
-    "cheerio": "^1.0.0",
-    "ical-generator": "^8.0.0"
-  }
-}
+module.exports = {
+
+    clubName:
+        "Boldklubben Frem",
+
+    clubId:
+        "1552",
+
+    competitionName:
+        "3. Division",
+
+    calendarName:
+        "Boldklubben Frem - 1. hold",
+
+    timezone:
+        "Europe/Copenhagen",
+
+    homeGround:
+        "Valby Idrætspark"
+
+};
