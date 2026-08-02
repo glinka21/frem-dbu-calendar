981bb9cf94780aefbb3663aaefb5db481f13b95a
glinka21 <oliver.bech21@gmail.com>
2026-08-02 09:34:53 +0200

    Add calendar creation functionality using ical-generator

---
+++ calendar.js
@@ -0,0 +1,136 @@
+const ical =
+require("ical-generator").default;
+
+const fs =
+require("fs");
+
+const config =
+require("./config");
+
+
+function createCalendar(matches){
+
+
+    const cal =
+    ical({
+
+        name:
+        config.calendarName,
+
+        timezone:
+        config.timezone
+
+    });
+
+
+
+    matches.forEach(match=>{
+
+
+        const home =
+        match.home.includes(
+            config.clubName
+        );
+
+
+        const icon =
+        home ? "🏠" : "🚌";
+
+
+        const start =
+        parseDbuDate(
+            match.date,
+            match.time
+        );
+
+
+        cal.createEvent({
+
+            id:
+            "frem-" +
+            match.id +
+            "@calendar",
+
+
+            start,
+
+
+            end:
+            new Date(
+                start.getTime()
+                +
+                7200000
+            ),
+
+
+            summary:
+            `${icon} ${match.home} - ${match.away}`,
+
+
+            location:
+            match.stadium,
+
+
+            description:
+`Boldklubben Frem
+3. Division
+
+DBU kamp:
+${match.id}`,
+
+
+            alarms:[
+
+                {
+                    type:
+                    "display",
+
+                    trigger:
+                    86400
+                }
+
+            ]
+
+        });
+
+
+    });
+
+
+    fs.writeFileSync(
+        "calendar.ics",
+        cal.toString()
+    );
+
+}
+
+
+
+function parseDbuDate(
+    date,
+    time
+){
+
+    const m =
+    date.match(
+        /(\d{2})-(\d{2})\s(\d{4})/
+    );
+
+
+    if(!m)
+        throw new Error(
+            "Ukendt dato: " + date
+        );
+
+
+    return new Date(
+        `${m[3]}-${m[2]}-${m[1]}T${time}:00`
+    );
+
+}
+
+
+module.exports =
+{
+    createCalendar
+};
